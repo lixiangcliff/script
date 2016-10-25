ecfb0ae9cda03a73b87a2174c230c69833d8d072
lixiangcliff <lixiang.cliff@gmail.com>
2016-10-24 18:09:53 -0700

    Fixed typo

--- bash/start_hubot.sh
+++ bash/start_hubot.sh
@@ -3,7 +3,7 @@
 slack_token=token=$(cat /home/cliff/.secret/slack_token)
 cd /home/cliff/repo/ccbot/
 
-tmux new -d -s ccbot "HUBOT_SLACK_TOKEN="&{slack_token}" ./bin/hubot --adapter slack"
+tmux new -d -s ccbot "HUBOT_SLACK_TOKEN="${slack_token}" ./bin/hubot --adapter slack"
 
 # to get into the tmux seesion again:
 # tmux a -t ccbot
