653c9f0ac466d658cebd5f4904f07ccd06412cf7
lixiangcliff <lixiang.cliff@gmail.com>
2016-10-30 19:09:45 -0700

    discard local changes before pulling

--- bash/update_repos.sh
+++ bash/update_repos.sh
@@ -4,6 +4,8 @@ dirs=$(ls -d /home/cliff/repo/*)
 for dir in ${dirs}
 do
 	cd ${dir};
+	git checkout -- .
+    echo -e "Discarded all local changes"
 	git pull
 	echo -e "Updated repo under: "${dir}"\n"
 done
