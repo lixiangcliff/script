145fca5c69de8850c70a47f6608fe517f6b6a29b
lixiangcliff <lixiang.cliff@gmail.com>
2016-11-03 16:05:02 -0700

    align output 3

--- bash/process_usage.sh
+++ bash/process_usage.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # http://www.tecmint.com/find-linux-processes-memory-ram-cpu-usage/
-ps -eo %mem:5,%cpu:5,cmd:80,user:6,pid:6,ppid:6 --sort=-%mem | head
+#ps -eo %mem:5,%cpu:5,cmd:80,user:6,pid:6,ppid:6 --sort=-%mem | head
+ps -axceo %mem:5,%cpu:5,cmd:10,user:5,pid:5,ppid:5 --sort=-%mem | head
