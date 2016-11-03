05637ccfd21da4600f957415604ba86e54254f2c
lixiangcliff <lixiang.cliff@gmail.com>
2016-11-03 15:28:07 -0700

    Made it simpler

--- bash/process_usage.sh
+++ bash/process_usage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # http://www.tecmint.com/find-linux-processes-memory-ram-cpu-usage/
-ps -eo uname,pid,ppid,cmd:80,%mem,%cpu --sort=-%mem | head
+ps -eo user,pid,ppid,cmd:80,%mem,%cpu --sort=-%mem | head
