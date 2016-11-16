fc3511e70460447a40f61d33d9678b8a340a0474
lixiangcliff <lixiang.cliff@gmail.com>
2016-11-16 11:10:00 -0800

    Enhanced update repos

--- bash/update_repos.sh
+++ bash/update_repos.sh
@@ -2,10 +2,41 @@
 
 dirs=$(ls -d /home/cliff/repo/*)
 for dir in ${dirs}
+#do
+#	cd ${dir};
+#	git checkout -- .
+#    echo -e "Discarded all local changes"
+#	git pull
+#	echo -e "Updated repo under: "${dir}"\n"
+#done
 do
 	cd ${dir};
+	echo -e "Updating repo under: "${dir}"\n"
 	git checkout -- .
-    echo -e "Discarded all local changes"
-	git pull
-	echo -e "Updated repo under: "${dir}"\n"
+    #echo -e "Discarded all local changes"
+
+    UPSTREAM=${1:-'@{u}'}
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    if [ $LOCAL = $REMOTE ]; then
+        echo "Already up-to-date"
+    elif [ $LOCAL = $BASE ]; then
+        #echo "Need to pull"
+
+        echo -e "\n"
+        git show
+        echo -e "\n"
+
+        git pull -q
+	    echo -e "Updated repo under: "${dir}
+    elif [ $REMOTE = $BASE ]; then
+        echo "Need to push"
+    else
+        echo "Diverged"
+    fi
+
+    echo -e "\n"
+
 done
