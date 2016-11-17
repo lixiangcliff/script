f22111551dc41831683befa84246f3ae6769f6db
lixiangcliff <lixiang.cliff@gmail.com>
2016-11-16 16:21:50 -0800

    test 20

--- bash/update_repos.sh
+++ bash/update_repos.sh
@@ -43,8 +43,8 @@ do
     echo -e "\n"
 done
 
-printf "\e[44mHello\e[0m\n"
-printf "\e[44mFinish updating repo under: "${dirs}"\e[0m\n"
+#printf "\e[44mHello\e[0m\n"
+#printf "\e[44mFinish updating repo under: "${dirs}"\e[0m\n"
 
 #printf "\033[31mHello\033[0m\n"
 #printf "\x1b[31mHello\x1b[0m\n"
