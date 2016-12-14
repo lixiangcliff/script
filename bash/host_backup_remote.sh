a068517a1c667c2bd819c09a190366097c3987b5
lixiangcliff <lixiang.cliff@gmail.com>
2016-12-14 13:44:30 -0800

    also keep backup copy locally

--- bash/host_backup_remote.sh
+++ bash/host_backup_remote.sh
@@ -14,26 +14,26 @@ ssh-add /home/cliff/.ssh/id_rsa_miwifi
 #save installed package list
 dpkg --get-selections | grep -v deinstall > /home/cliff/installed_packages.txt
 
-# What to backup. 
-backup_files="/home /var/backups/jenkins /var/spool/mail /var/spool/cron /etc /root /boot /var/www"
-exclude_files="/home/cliff/.cache"
-
 # Where to backup to.
 dest="/extdisks/sda5/ubuntu-backup"
+local_dest="/home/cliff/ubuntu-backup"
+
+# What to backup. 
+backup_files="/home /var/backups/jenkins /var/spool/mail /var/spool/cron /etc /root /boot /var/www"
+exclude_files="/home/cliff/.cache "$local_dest
 
 #Delete all tar files that is older than 10 days
 #rmFilename=`date --date='10 days ago' +%Y-%m-%d`
 #rm $dest/"*"$rmFilename"*"
 
 #Delete all .tgz files that is older than x days
-#ssh root@miwifi
-#cd ${dest}
-#STORE_INTERVAL=7
-#files_to_rm=`find ${dest} -type f -mtime +${STORE_INTERVAL} -name '*.tgz'`
-#find ${dest} -type f -mtime +${STORE_INTERVAL} -name '*.tgz' -exec rm -- {} \;
-#echo
-#echo -e "Remove files older than "${STORE_INTERVAL}" days: "${files_to_rm}
-#exit
+#this is to remove local backup, remote backup removal needs to be added later
+
+STORE_INTERVAL=7
+files_to_rm=`find ${local_dest} -type f -mtime +${STORE_INTERVAL} -name '*.tgz'`
+find ${local_dest} -type f -mtime +${STORE_INTERVAL} -name '*.tgz' -exec rm -- {} \;
+echo
+echo -e "On local, remove files older than "${STORE_INTERVAL}" days: "${files_to_rm}
 
 # Create archive filename.
 day=$(date +%A)
@@ -43,26 +43,26 @@ archive_file="$hostname-$timestamp.tgz"
 
 # list large files:
 echo
-echo "Top largest size dirs:"
+echo "Top largest size dirs to backup:"
 du -Sh  $backup_files | sort -hr | head -30 | grep -v $exclude_files
 
 # Print start status message.
 echo
 echo "Backing up $backup_files to $dest/$archive_file"
 echo "Excluding dirs: $exclude_files"
-date
 echo
 
-# Backup the files using tar.
-temp_dir="/home/cliff/temp/"
-tar --exclude=${exclude_files} -zcf $temp_dir$archive_file $backup_files
-scp $temp_dir$archive_file "root@miwifi":$dest
-rm $temp_dir$archive_file
+# Backup the files locally and scp to remote.
+tar --exclude=${exclude_files} -zcf $local_dest$archive_file $backup_files
+scp $local_dest$archive_file "root@miwifi":$dest
+
 
 # Print end status message.
 echo
 echo "Backup finished"
-date
 
 # Long listing of files in $dest to check file sizes.
+echo -e "\nOn local:"
+ls -rlh $local_dest
+echo -e "\nOn remote:"
 ssh root@miwifi "du -sh "$dest"; ls -rlh "$dest
