fb07f4783b8615ccde168a862fc08864cc1978a3
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-03 15:17:52 -0800

    Added more info

--- bash/check_jenkins.sh
+++ bash/check_jenkins.sh
@@ -5,5 +5,5 @@ jenkins_port=$(cat /home/cliff/.secret/jenkins_port)
 webhook_url=$(cat /home/cliff/.secret/webhook_url)
 statusCode=$(curl -I -s -L ${public_ip}:${jenkins_port} | grep "HTTP/1.1" | awk '{print $2}')
 if [[ $statusCode != "403" ]]; then
-    ./post_to_slack.sh -t "Jenkins is Down!" -b "Please check jenkins service." -c "ccjenkins" -u ${webhook_url} -r "danger"
+    ./post_to_slack.sh -t "Jenkins is Down!" -b "Status code is: ${statusCode}" -c "ccjenkins" -u ${webhook_url} -r "danger"
 fi
