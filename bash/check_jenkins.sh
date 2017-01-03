c27a31e37296c86aa878fd3eb067a709a8b88efd
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-03 15:22:38 -0800

    Added reponse status code

--- bash/check_jenkins.sh
+++ bash/check_jenkins.sh
@@ -3,7 +3,13 @@
 public_ip=$(cat /home/cliff/.secret/public_ip)
 jenkins_port=$(cat /home/cliff/.secret/jenkins_port)
 webhook_url=$(cat /home/cliff/.secret/webhook_url)
+
 statusCode=$(curl -I -s -L ${public_ip}:${jenkins_port} | grep "HTTP/1.1" | awk '{print $2}')
+
+if [[ $statusCode == "" ]]; then
+    $statusCode="ERR_EMPTY_RESPONSE"
+fi
+
 if [[ $statusCode != "403" ]]; then
-    ./post_to_slack.sh -t "Jenkins is Down!" -b "Status code is: ${statusCode}" -c "ccjenkins" -u ${webhook_url} -r "danger"
+    ./post_to_slack.sh -t "Jenkins is Down!" -b "Response status code is: ${statusCode}" -c "ccjenkins" -u ${webhook_url} -r "danger"
 fi
