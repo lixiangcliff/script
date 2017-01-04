93b657495cfbbf4fa40f3cef82b9d089f924c2ce
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-04 13:17:27 -0800

    handle relative script path

--- bash/check_jenkins.sh
+++ bash/check_jenkins.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cur_dir=`dirname $0`
+
 public_ip=$(cat /home/cliff/.secret/public_ip)
 jenkins_port=$(cat /home/cliff/.secret/jenkins_port)
 webhook_url=$(cat /home/cliff/.secret/webhook_url)
@@ -11,5 +13,5 @@ if [[ $statusCode == "" ]]; then
 fi
 
 if [[ $statusCode != "403" ]]; then
-    ./post_to_slack.sh -t "Jenkins is Down!" -b "Response status code is: ${statusCode}" -c "ccjenkins" -u ${webhook_url} -r "danger"
+    $cur_dir/post_to_slack.sh -t "Jenkins is Down!" -b "Response status code is: ${statusCode}" -c "ccjenkins" -u ${webhook_url} -r "danger"
 fi
