cb6940e67be664d0ff30c422349e09ddbdedee8a
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-04 15:53:34 -0800

    better log

--- bash/check_jenkins.sh
+++ bash/check_jenkins.sh
@@ -19,7 +19,7 @@ RETRY_INTERVAL_IN_SECOND=60
 
 get_status_code
 if [[ $statusCode != "403" ]]; then
-    $cur_dir/post_to_slack.sh -t "Jenkins is Down!" -b "Response status code is: ${statusCode}. Now restart jenkins service and retry in ${RETRY_INTERVAL_IN_SECOND} seconds" -c "ccjenkins" -u ${webhook_url} -r "danger"
+    $cur_dir/post_to_slack.sh -t "Jenkins is down!" -b "Response status code is: ${statusCode}. Now restart jenkins service and retry in ${RETRY_INTERVAL_IN_SECOND} seconds" -c "ccjenkins" -u ${webhook_url} -r "danger"
     sudo service jenkins restart
     sleep ${RETRY_INTERVAL_IN_SECOND}
     get_status_code
