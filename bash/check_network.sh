a4f391445c4e325bb8b228df008ef0a6d6c26f32
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-26 15:04:08 -0800

    better log

--- bash/check_network.sh
+++ bash/check_network.sh
@@ -3,8 +3,8 @@
 status_code=$(curl -I -s -L "https://www.google.com/" | grep "HTTP/1.1" | awk '{print $2}')
 
 if [[ $status_code != "200" ]]; then
-    printf "\nNetwork is down!\n"
+    printf "\nNetwork is down!\n\n"
     exit 1
 else
-    printf "\nNetwork is OK!\n"
+    printf "\nNetwork is OK!\n\n"
 fi
