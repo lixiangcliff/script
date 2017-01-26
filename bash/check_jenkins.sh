192227cbc846f1cfc447885711a29af2a5067485
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-26 14:29:58 -0800

    better naming

--- bash/check_jenkins.sh
+++ bash/check_jenkins.sh
@@ -6,25 +6,25 @@ public_ip=$(cat /home/cliff/.secret/public_ip)
 jenkins_port=$(cat /home/cliff/.secret/jenkins_port)
 webhook_url=$(cat /home/cliff/.secret/webhook_url)
 
-statusCode=""
+status_code=""
 
 get_status_code() {
-    statusCode=$(curl -I -s -L ${public_ip}:${jenkins_port} | grep "HTTP/1.1" | awk '{print $2}')
-    if [[ $statusCode == "" ]]; then
-        statusCode="ERR_EMPTY_RESPONSE"
+    status_code=$(curl -I -s -L ${public_ip}:${jenkins_port} | grep "HTTP/1.1" | awk '{print $2}')
+    if [[ $status_code == "" ]]; then
+        status_code="ERR_EMPTY_RESPONSE"
     fi
 }
 
 RETRY_INTERVAL_IN_SECOND=60
 
 get_status_code
-if [[ $statusCode != "403" ]]; then
-    $cur_dir/post_to_slack.sh -t "Jenkins is down!" -b "Response status code is: ${statusCode}. Now restart jenkins service and retry in ${RETRY_INTERVAL_IN_SECOND} seconds" -c "ccjenkins" -u ${webhook_url} -r "danger"
+if [[ $status_code != "403" ]]; then
+    $cur_dir/post_to_slack.sh -t "Jenkins is down!" -b "Response status code is: ${status_code}. Now restart jenkins service and retry in ${RETRY_INTERVAL_IN_SECOND} seconds" -c "ccjenkins" -u ${webhook_url} -r "danger"
     sudo service jenkins restart
     sleep ${RETRY_INTERVAL_IN_SECOND}
     get_status_code
-    if [[ $statusCode != "403" ]]; then
-        $cur_dir/post_to_slack.sh -t "After restarting, Jenkins is still Down!" -b "Response status code is: ${statusCode}." -c "ccjenkins" -u ${webhook_url} -r "danger"
+    if [[ $status_code != "403" ]]; then
+        $cur_dir/post_to_slack.sh -t "After restarting, Jenkins is still Down!" -b "Response status code is: ${status_code}." -c "ccjenkins" -u ${webhook_url} -r "danger"
     else
         $cur_dir/post_to_slack.sh -t "Jenkins is up now!" -b "After restarting jenkins service is running again" -c "ccjenkins" -u ${webhook_url} -r "good"
     fi
