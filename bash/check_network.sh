b2d005d92a38b6049e4c702ba7435991690b8d48
lixiangcliff <lixiang.cliff@gmail.com>
2017-01-29 00:50:47 -0800

    Added restart network cmd

--- bash/check_network.sh
+++ bash/check_network.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
-status_code=$(curl -I -s -L "https://www.google.com/" | grep "HTTP/1.1" | awk '{print $2}')
+status_code=""
 
+get_status_code() {
+    status_code=$(curl -I -s -L "https://www.google.com/" | grep "HTTP/1.1" | awk '{print $2}')
+}
+
+RETRY_INTERVAL_IN_SECOND=5
+
+get_status_code
 if [[ $status_code != "200" ]]; then
-    printf "\nNetwork is down!\n\n"
-    exit 1
+    printf "\nNetwork is down! \nTry to restart network now! \n"
+    sudo /etc/init.d/network-manager restart
+    sleep ${RETRY_INTERVAL_IN_SECOND}
+    get_status_code
+    if [[ $status_code != "200" ]]; then
+        printf "\nAfter restarting, network is still down!\n"
+        exit 1
+    else
+        printf "\nAfter restarting, network is OK now!\n"
+    fi
 else
     printf "\nNetwork is OK!\n\n"
 fi
