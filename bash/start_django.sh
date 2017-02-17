c6f3b0441b4d1b03c39e14f9692af3e19c3d00b9
lixiangcliff <lixiang.cliff@gmail.com>
2017-02-17 15:57:10 -0800

    let it work for diff env

--- bash/start_django.sh
+++ bash/start_django.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+env=$(cat /home/cliff/.secret/env)
+django_ip=$(cat /home/cliff/.secret/django_ip)
+django_port=$(cat /home/cliff/.secret/django_port)
+manage_path="/home/cliff/repo/ccfam/src/manage.py runserver "
 
-tmux new -d -s django "/home/cliff/repo/website/manage.py runserver 192.168.31.199:8000"
+tmux new -d -s django $manage_path$django_ip":"$django_port
 
 echo "Start django tmux session"
 
